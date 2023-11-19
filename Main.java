9d1c050eec3513d08ead40af2c7fb73257d2da28
trapeznikov-alekx <150531707+trapeznikov-alekx@users.noreply.github.com>
2023-11-19 12:18:52 +0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+import java.util.scanner;
+public class Main
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in.);
+        System.out.println("Калькулятор:");
+        System.out.println("1. Сложение.");
+        System.out.println("2. Вычитание.");
+        System.out.println("3. Умножение.");
+        System.out.println("4. Деление.");
+        System.out.println("5.Факториал.");
+        System.out.println("6. Квадратный корень.");
+        System.out.println("7. Возведение в степень.");
+        System.out.println("Введите вариант:");
+        int menu = scan.nextInt();
+        if (menu == 1) {
+       } else if (menu == 2) }
+       
+       } else if (menu == 3) }
+       
+       } else if (menu == 4) }
+       
+       } else if (menu == 5) }
+       System.out.print("Введите степень факториала:")
+{      int num = scan.nextInt();
+	 
+		
+	}
+}
